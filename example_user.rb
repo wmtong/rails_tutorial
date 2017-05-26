f14bf391f6976c075d86574562d146e887f08926
William Tong <william.tong@cerner.com>
2017-05-26 14:50:42 -0500

    Make layout changes

---
+++ example_user.rb
@@ -0,0 +1,17 @@
+class User
+  attr_accessor :first_name, :last_name, :email
+
+  def initialize(attributes = {})
+    @first_name = attributes[:first_name]
+    @last_name = attributes[:last_name]
+    @email = attributes[:email]
+  end
+
+  def formatted_email
+    "#{@first_name} #{@last_name} <#{@email}>"
+  end
+
+  def full_name
+    "#{@first_name} #{@last_name}"
+  end
+end
